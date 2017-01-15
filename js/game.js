7dfe9a51a17a525276b0174acede1b8da57a0731
bdrupieski <brian.drupieski@gmail.com>
2017-01-15 17:48:04 -0500

    paddle physics and scoring

--- js/game.js
+++ js/game.js
@@ -12,12 +12,19 @@ const Pong = (function() {
     const paddleHeight = 80;
     const paddleDepth = 10;
     const paddleQuality = 1;
-    const paddleSpeed = 4;
+    const paddleSpeed = 10;
+
+    const ballSpeed = 5;
 
     let ballDirX = 1;
     let ballDirY = 1;
 
-    function setup(canvasId) {
+    let player1Score = 0;
+    let player2Score = 0;
+
+    function setup(canvasId, player1ScoreId, player2ScoreId) {
+        that.player1ScoreId = player1ScoreId;
+        that.player2ScoreId = player2ScoreId;
 
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(sceneWidth, sceneHeight);
@@ -53,8 +60,21 @@ const Pong = (function() {
     function draw() {
         that.renderer.render(that.scene, that.camera);
         requestAnimationFrame(draw);
-        ballPhysics(that.ball, function() {}, function() {});
+        ballPhysics(that.ball, playerScored, cpuScored);
         playerPaddleMovement(that.paddle1);
+        opponentPaddleMovement(that.paddle2);
+        paddlePhysics(that.paddle1);
+        paddlePhysics(that.paddle2);
+    }
+
+    function playerScored() {
+        player1Score++;
+        document.getElementById(that.player1ScoreId).textContent = player1Score;
+    }
+
+    function cpuScored() {
+        player2Score++;
+        document.getElementById(that.player2ScoreId).textContent = player2Score;
     }
 
     function buildCamera() {
@@ -120,16 +140,14 @@ const Pong = (function() {
 
     function ballPhysics(ball, onCpuScore, onPlayerScore) {
 
-        const ballSpeed = 2;
-
         if (ball.position.x <= -fieldWidth / 2) {
             resetBall();
-            onCpuScore();
+            onPlayerScore();
         }
 
         if (ball.position.x >= fieldWidth / 2) {
             resetBall();
-            onPlayerScore();
+            onCpuScore();
         }
 
         if (ball.position.y <= -fieldHeight / 2) {
@@ -153,24 +171,30 @@ const Pong = (function() {
     function playerPaddleMovement(paddle) {
         let paddle1DirY = 0;
 
-        if (Key.isDown(Key.A)) {
-            if (paddle.position.y < fieldHeight * 0.45) {
-                paddle1DirY = paddleSpeed * 0.5;
-            }
-            else {
-                paddle.scale.z += (10 - paddle.scale.z) * 0.2;
-            }
-
-        } else if (Key.isDown(Key.D)) {
-            if (paddle.position.y > -fieldHeight * 0.45) {
-                paddle1DirY = -paddleSpeed * 0.5;
-            }
-            else {
-                paddle.scale.z += (10 - paddle.scale.z) * 0.2;
-            }
+        if (Key.isDown(Key.A) && paddle.position.y < fieldHeight * 0.45) {
+            paddle1DirY = paddleSpeed * 0.5;
+        } else if (Key.isDown(Key.D) && paddle.position.y > -fieldHeight * 0.45) {
+            paddle1DirY = -paddleSpeed * 0.5;
         }
 
         paddle.position.y += paddle1DirY;
+    }
+
+    function opponentPaddleMovement(paddle) {
+        const paddle2DirY = (ball.position.y - paddle.position.y) * 0.1;
+
+        if (Math.abs(paddle2DirY) <= paddleSpeed) {
+            paddle.position.y += paddle2DirY;
+        }
+        else {
+            if (paddle2DirY > paddleSpeed) {
+                paddle.position.y += paddleSpeed;
+            }
+            else if (paddle2DirY < -paddleSpeed) {
+                paddle.position.y -= paddleSpeed;
+            }
+        }
+        paddle.scale.y += (1 - paddle.scale.y) * 0.2;
     }
 
     function resetBall() {
@@ -178,6 +202,29 @@ const Pong = (function() {
         ball.position.y = 0;
 
         ballDirX = -ballDirX;
+    }
+
+    function paddlePhysics(paddle) {
+
+        function boundsCheck() {
+            if (paddle == that.paddle1) {
+                return ballDirX < 0;
+            } else {
+                return ballDirX > 0;
+            }
+        }
+
+        if (ball.position.x <= paddle.position.x + paddleWidth &&
+            ball.position.x >= paddle.position.x) {
+
+            if (ball.position.y <= paddle.position.y + paddleHeight / 2 &&
+                ball.position.y >= paddle.position.y - paddleHeight / 2) {
+
+                if (boundsCheck()) {
+                    ballDirX = -ballDirX;
+                }
+            }
+        }
     }
 
     return {
